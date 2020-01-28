381fa42cdbacb2560f19c77f06508209a0cb8491
Lionel Mace <lionel.mace@gmail.com>
2020-01-28 01:35:11 +0100

    yarn and commented sed

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ export SHELLOPTS
 touch -t 201811111111.11 workshop/*
 
 # patch gitbook so START_TIME remains the same in the generated html
-sed -i 's/var START_TIME = new Date();/var START_TIME = new Date(2018, 10, 11, 11, 11, 11);/g' ~/.gitbook/versions/3.2.3/lib/gitbook.js
+#sed -i 's/var START_TIME = new Date();/var START_TIME = new Date(2018, 10, 11, 11, 11, 11);/g' ~/.gitbook/versions/3.2.3/lib/gitbook.js
 
 # build the book
 ./node_modules/.bin/gitbook build
